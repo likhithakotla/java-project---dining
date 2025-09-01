070ceb1fa5ac25a0ceba8559675fbd88bbf7e099
likhithakotla <lkotla@buffalo.edu>
2025-08-31 21:34:22 -0400

    package change

--- restaurant-service/src/main/java/com/example/restaurant/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/example/restaurant/RestaurantServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.restaurant;
+package main.java.com.example.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
